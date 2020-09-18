6a32b7209ca81a8003c06725624c796da55ee7e5
raghu <raghumina11@gmail.com>
2020-09-18 21:49:40 +0530

    Update If_Else_Problems.py

--- If_Else_Problems.py
+++ If_Else_Problems.py
@@ -5,17 +5,15 @@
 # but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
 
 def leap_year(year):
-    if year % 400 == 0 and year % 4 == 0 and year % 100 and year % 100 != 0:
-
-
-
-
-
-        return "LEAP_YEAER"
+    if year % 400 == 0:
+        return "LeaP yEAR"
+    elif year % 100 == 0:
+        return "NoT LEAP yEAR "
+    elif year % 4 == 0:
+        return "LeaP yEAR"
     else:
         print("Not Leap Year")
 
 
 year = int(input("Please enter a year "))
 print(leap_year(year))
-
